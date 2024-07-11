a6677064ec5e479428f91a794831b09c505d7841
semiadult <julius.schroeder@stud.abk-stuttgart.de>
2024-07-11 18:10:29 +0200

    Add prompt to change grid size

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 "use strict"
 
-
-
-//function to create divs for row with column nested inside.
-function makeGrid(number) {
-    const container = document.querySelector("#container");
-    if (number < 100) {
-    for (let i=0; i < number; i++) {
-        let row = document.createElement("div");
-        row.classList.add("row");
-        container.appendChild(row);
-
-        for (let i=0; i < number; i++) {
-            let column = document.createElement("div");
-            column.classList.add("column");
-            row.appendChild(column);
-            column.addEventListener("mouseenter", (event) => {
-                column.setAttribute("style", "background: black");
-            });
-            }
-        }
-    }
-}
-makeGrid(16);
-
-let buttonContainer = document.querySelector("#buttonContainer");
+let header = document.querySelector("#header");
 let resetButton = document.createElement("button");
 resetButton.classList.add("reset");
 resetButton.textContent= "Erase!";
-buttonContainer.appendChild(resetButton);
-//make button to reset the grid and choose different dimensions
-resetButton.addEventListener("click", () => {
+header.appendChild(resetButton)
+
+//function to create divs for row with column nested inside.
+function makeGrid(number) {
+    const container = document.querySelector  ("#container");
+        for (let i=0; i < number; i++) {
+            let row = document.createElement("div");
+            row.classList.add("row");
+            container.appendChild(row);
+
+            for (let i=0; i < number; i++) {
+                let column = document.createElement("div");
+                column.classList.add("column");
+                row.appendChild(column);
+                column.addEventListener("mouseenter", (event) => {
+                column.setAttribute("style", "background: black");
+                });
+            }
+        }
+    
+    }
+makeGrid(16);
+
+
+function removeGrid() {
+resetButton.addEventListener("mousedown", () => {
     container.replaceChildren();
-    makeGrid(16);
+    let size = prompt ("How many squares per side? Maximum is 100");
+    size = parseInt(size);
+    if (size <= 100 && size !== null) {
+    makeGrid(size);
+    } else makeGrid(16);
 });
+}
+removeGrid();
